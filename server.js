b7331ba91c16d94f83926cb197c28a541fc2cf21
Brett <brett.sandusky@gmail.com>
2017-02-02 18:30:26 +0000

    Parse params from url

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require("express")
+var validate = require("./validate")
 var app = express()
 
+var response = {unix: null, natural: null}
+
 app.get('/', (req, res) => {
-    res.send("Hello world")
+    res.send(response)
 })
 
-app.listen(8080, () => {
-    console.log("Server running on port 8080")
+app.get('/:timestamp', (req, res) => {
+    
+    if (validate(req.params.timestamp)) {
+        res.send(req.params.timestamp)
+    } else {
+        res.send("Not a valid date")
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT)
 })
---
+++ validate.js
@@ -0,0 +1,8 @@
+const validate = (timestamp) => {
+    //var date = new Date(timestamp)
+    //return (date instanceof Date && !isNaN(date.valueOf()))
+    
+    return true
+}
+
+module.exports = validate
